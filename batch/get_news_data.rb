56e7d836e555263a52fb120e1dcb8e459bf79779
ksugawara61 <katsuo.com61@gmail.com>
2016-09-16 12:36:50 +0100

    スクレイピング時にエラーが発生しないように修正

--- batch/get_news_data.rb
+++ batch/get_news_data.rb
@@ -27,23 +27,27 @@ module GetNewsData
 
     rss = RSS::Parser.parse(filename)
     rss.items.each{|item|
-      # サムネイル画像のパスを取得するためHTMLを取得
+      # RSSのリンクのHTMLを取得
       charset = nil
       html = open(item.link) do |f|
         charset = f.charset
         f.read
       end
       
+      # HTMLからニュースの各要素をスクレイピング
       doc = Nokogiri::HTML.parse(html, nil, charset)
-      title = doc.xpath('//div[@class="gn-topics"]/h2/a').text
-      siteName = doc.xpath('//p[@class="topics-news-source margin-bottom15"]/a').text
-      thumbnail = doc.xpath('//div[@class="topics-thumbs"]/p/a/img').attribute("src")
+      tmp = doc.xpath('//div[@class="gn-topics"]/h2/a')
+      title = tmp.nil? ? "" : tmp.text
+      tmp = doc.xpath('//p[@class="topics-news-source margin-bottom15"]/a')
+      siteName = tmp.nil? ? "" : tmp.text
+      tmp = doc.xpath('//div[@class="topics-thumbs"]/p/a/img')
+      thumbnail = tmp.empty? ? "" : tmp.attribute("src").value
 
-      puts title
-      puts item.link
-      puts siteName
-      puts thumbnail
-      puts item.pubDate
+      p title
+      p item.link
+      p siteName
+      p thumbnail
+      p item.pubDate
       puts
     }
 
