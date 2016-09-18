639f9cbabeff13e54f9612e402317e06c088c048
ksugawara61 <katsuo.com61@gmail.com>
2016-09-18 05:00:11 +0100

    コメントアウトしていたスタブの箇所を削除

--- lib/news_adapter.rb
+++ lib/news_adapter.rb
@@ -38,23 +38,6 @@ module NewsAdapter
       NewsLogger.err_logging e
     end
 
-    # news = [
-    #         {
-    #           "title" => "G 沢村追いつかれ延長で競り負け",
-    #           "link"  => "http://news.goo.ne.jp/topstories/sports/190/3f882faf88f8d827747054999af32881.html?fr=RSS",
-    #           "siteName" => "gooニュース",
-    #           "thumbnail" => "http://news.goo.ne.jp/picture/sports/sanspo-gia1609140004.html",
-    #           "pubDate" => "2016-09-14 22:38:43"
-    #         },
-    #         {
-    #           "title" => "G 沢村追いつかれ延長で競り負け",
-    #           "link"  => "http://news.goo.ne.jp/topstories/sports/190/3f882faf88f8d827747054999af32881.html?fr=RSS",
-    #           "siteName" => "gooニュース",
-    #           "thumbnail" => "http://news.goo.ne.jp/picture/sports/sanspo-gia1609140004.html",
-    #           "pubDate" => "2016-09-14 22:38:43"
-    #         }
-    #        ]
-
     return news
   end
 
